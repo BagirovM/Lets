3068014b7bcd42b54ed1df38fb608b2e06ff6035
BagirovM <109734813+BagirovM@users.noreply.github.com>
2022-08-19 23:09:24 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from "react";
-import {Routes, Route} from 'react-router-dom'
-import Contact from "./routes/Contact";
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-import Home from "./routes/Home"
-import Pricing from "./routes/Pricing"
-import Training from "./routes/Training";
-import ImageGallery from 'react-image-gallery';
 
-function App() {
+import Home from './routes/Home'
+import Pricing from './routes/Pricing'
+import Navbar from './componets/Navbar'
+import Training from './routes/Training'
+import Contact from './routes/Contact'
+
+
+const App = () => {
   return (
-  
-      <>
-       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/training" element={<Training/>} />
-        <Route path="/contact" element={<Contact/>} />
-       </Routes>
-       </>
-    
-  );
+    <>
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/pricing' element={<Pricing/>}/>
+        <Route path='/training' element={<Training/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
