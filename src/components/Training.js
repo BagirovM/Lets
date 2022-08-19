773a2099f0b3e443c368175e1d6e548c4699d9d3
BagirovM <109734813+BagirovM@users.noreply.github.com>
2022-08-19 22:59:56 +0400

    Update Training.js

--- src/components/Training.js
+++ src/components/Training.js
@@ -1,37 +1,30 @@
 import React from 'react'
 import './TrainingStyles.css'
 
-import {Link} from 'react-router-dom'
-.
-import Pod from '../assets/pod.jpg'
-import Pod2 from '../assets/pod2.jpg'
-import Pod3 from '../assets/pod3.webp'
+import { Link } from 'react-router-dom'
 
-const Training = () => {
-    return (
-      
-        <div className='training'>
-            <div className='left'>
-                <h1>Training</h1>
-                <p>Thorough training is a necessity when traveling to space. We offer all inclusive training for pre-flight and in-flight scenarios.</p>
-                <Link to='/contact'><button className='btn'>Contact</button></Link>
-            </div>
-            <div className='right'>
-                <div className='img-container'>
-                    <div className='image-stack top'>
-                        <img src={Pod} className='img' alt='' />
-                    </div>
-                    <div className='image-stack bottom'>
-                        <img src={Pod2} className='img' alt='' />
-                    </div>
-                    <div className='image-stack senter '>
-                        <img src={Pod3} className='img' alt='' />
-                    </div>
+import Cosmonaut from '../assets/cosmonaut.jpg'
+import moon from '../assets/moon.jpg'
+function Training() {
+  return (
+    <div className='training'>
+        <div className='left'>
+            <h1>Training</h1>
+            <p>"GLS TRVL" turizm şirkəti 2001-ci ildə yaradılıb. Biz yüksək keyfiyyətli turizm xidmətlərinin göstərilməsi üzrə ixtisaslaşmışıq və sizə bizim xidmətimizdən həqiqətən həzz alacağınıza söz veririk. Sizə keyfiyyətli istirahət və sənədlərin hazırlanmasında və xaricə səfərlərdə əla xidmətə zəmanət veririk.</p>
+            <Link to='/contact'><button className='button btc'>Contact</button></Link>
+        </div>
+        <div className='right'>
+            <div className='img-container'>
+                <div className='image-stack top'>
+                    <img src={moon} className='img' alt=''/>
+                </div>
+                <div className='image-stack bottom'>
+                    <img src={Cosmonaut} className='img' alt=''/>   
                 </div>
             </div>
         </div>
-    
-    )
+    </div>
+  )
 }
 
 export default Training
