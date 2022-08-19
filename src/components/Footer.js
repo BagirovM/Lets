d7f02e8d08f5a1c4dcb85d2be767530d44e2e3fe
BagirovM <109734813+BagirovM@users.noreply.github.com>
2022-08-19 22:40:45 +0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,39 @@
 import React from 'react'
 import './FooterStyles.css'
-import { FaFacebook, FaLinkedin, FaMailBulk, FaPhone, FaSearchLocation, FaTwitter } from 'react-icons/fa'
+import {FaFacebook, FaMailBulk, FaPhone, FaSearchLocation, FaTwitter} from 'react-icons/fa'
 
 const Footer = () => {
-    return (
-        <div className='footer'>
-            <div className='footer-container'>
-                <div className='left'>
-                    <div className='location'>
-                        <FaSearchLocation size={20} style={{ color: '#ffffff', marginRight: '2rem' }} />
-                        <div>
-                            <p>Samir Samirov St.</p>
-                            <h4>Baku, Aze</h4>
-                        </div>
-                    </div>
-                    <div className='phone'>
-                        <h4><FaPhone size={20} style={{ color: '#ffffff', marginRight: '2rem' }} />(+994)50-210-88-92</h4>
-                    </div>
-                    <div className='email'>
-                        <h4><FaMailBulk size={20} style={{ color: '#ffffff', marginRight: '2rem' }} /> trips@galaxy.com</h4>
-                    </div>
+  return (
+    <div className='footer'>
+        <div className='footer-container'>
+            <div className='left'>
+                <div className='location'>
+                    <FaSearchLocation size={20} style={{color:'#ffffff', marginRight:'2rem'}}/>
+                <div>
+                    <p>123 Acne St.</p>
+                    <h4>Houston, Tx</h4>
                 </div>
-                <div className='right'>
-                    <h4>About the company</h4>
-                    <p>"Our company GLX TRVL is the official company in Azerbaijan for space tourism. There are a lot of profitable tours in our company .We send more than 1,000 people into space a year. You can fully trust us!"</p>
-                    <div className='social'>
-                        <FaFacebook size={30} style={{ color: '#ffffff', marginRight: '1rem' }} />
-                        <FaTwitter size={30} style={{ color: '#ffffff', marginRight: '1rem' }} />
-                        <FaLinkedin size={30} style={{ color: '#ffffff', marginRight: '1rem' }} />
-                    </div>
-                </div>
+            </div>
+            <div className='phone'>
+                <h4><FaPhone size={20} style={{color:'#ffffff', marginRight:'2rem'}}/> (+994)50-210-88-92</h4>
+            </div>
+            <div className='email'>
+                <h4><FaMailBulk size={20} style={{color:'#ffffff', marginRight:'2rem'}}/>trips@galaxy.com</h4>
+            </div>
+        </div>
+        <div className='right'>
+            <h4>About the company </h4>
+            <p>Welcome to our company GLX TRVL, we are glad to see you. Our company has been serving for more than 10 years. Every year we have more than 1,000+ people who have flown into space with us. We offer you the cheapest prices for today. To get more information please contact dreams</p>
+            <div className='serial'>
+                <FaFacebook  size={30} style={{color:'#ffffff', marginRight:'1rem'}}/>
+                <FaTwitter  size={30} style={{color:'#ffffff', marginRight:'1rem'}}/>
+               
 
             </div>
         </div>
-    )
+    </div>
+</div>
+  )
 }
 
 export default Footer
