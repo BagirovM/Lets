5d9ad9c55305ed01a0d0e8778bd09d94855c58a1
BagirovM <109734813+BagirovM@users.noreply.github.com>
2022-08-19 13:27:11 +0400

    Update Training.js

--- src/components/Training.js
+++ src/components/Training.js
@@ -2,29 +2,35 @@ import React from 'react'
 import './TrainingStyles.css'
 
 import {Link} from 'react-router-dom'
-
-import pod from '../assets/pod.jpg'
-import pod2 from '../assets/pod2.jpg'
+.
+import Pod from '../assets/pod.jpg'
+import Pod2 from '../assets/pod2.jpg'
+import Pod3 from '../assets/pod3.webp'
 
 const Training = () => {
     return (
+      
         <div className='training'>
             <div className='left'>
-                <h1 className='h1'>Training</h1>
-                <p className='Tra'>Thorough training is a necessity when traveling to space. We offer all inclusive training for pre-flight and in-flight scenarios.</p>
+                <h1>Training</h1>
+                <p>Thorough training is a necessity when traveling to space. We offer all inclusive training for pre-flight and in-flight scenarios.</p>
                 <Link to='/contact'><button className='btn'>Contact</button></Link>
-            
+            </div>
             <div className='right'>
                 <div className='img-container'>
                     <div className='image-stack top'>
-                        <img src={pod} className='img' alt='' />
+                        <img src={Pod} className='img' alt='' />
                     </div>
                     <div className='image-stack bottom'>
-                        <img src={pod2} className='img' alt='' />
+                        <img src={Pod2} className='img' alt='' />
+                    </div>
+                    <div className='image-stack senter '>
+                        <img src={Pod3} className='img' alt='' />
                     </div>
                 </div>
             </div>
-        </div></div>
+        </div>
+    
     )
 }
 
