c349f99e88738084209e7909cf4a55923cde981c
BagirovM <109734813+BagirovM@users.noreply.github.com>
2022-08-19 22:58:32 +0400

    Update Pricing.js

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,43 +1,45 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import{Link} from 'react-router-dom'
 import './PricingStyles.css'
 
-function Pricing() {
+const Pricing = () => {
   return (
     <div className='pricing'>
         <div className='card-container'>
             <div className='card'>
-                <h3>-Basic-</h3>
+                <h1>-Basic-</h1>
                 <span className='bar'></span>
                 <p className='btc'>1 BTC</p>
-                <p>3 Days</p>
-                <p>Views</p>
-                <p>Featured</p>
-                <p className='p'>Private Quarters</p>
-                <Link to='/contact' className='btc book'>Book</Link>
+                <p>-3 Days-</p>
+                <p>-Views-</p>
+                <p>-Feature-</p>
+                <p>-Private Quarters-</p>
+                <Link to='/contact' className='btn'>Book</Link>
+
             </div>
             <div className='card'>
-                <h3>-Suite-</h3>
+                <h1>-Suite-</h1>
                 <span className='bar'></span>
                 <p className='btc'>2 BTC</p>
-                <p>3 Days</p>
-                <p>Views</p>
-                <p>Featured</p>
-                <p className='p'>Private Quarters</p>
-                <Link to='/contact' className='btc'>Book</Link>
+                <p>-5 Days-</p>
+                <p>-Views-</p>
+                <p>-Video-</p>
+                <p>-Private Quarters-</p>
+                <Link to='/contact' className='btn'>Book</Link>
+
             </div>
             <div className='card'>
-                <h3>-VIP-</h3>
+                <h1>-VIP-</h1>
                 <span className='bar'></span>
-                <p className='btc'>3 BTC</p>
-                <p>3 Days</p>
-                <p>Views</p>
-                <p>Featured</p>
-                <p className='p'>Private Quarters</p>
-                <Link to='/contact' className='btc'>Book</Link>
-            </div>
-            
+               <p className='btc'>3 BTC</p>
+                <p>-9 Days-</p>
+                <p>-Views-</p>
+                <p>-Video-</p>
+                <p>-Photo-</p>
+                <p>-Private Quarters-</p>
+                <Link to='/contact' className='btn'>Book</Link>
 
+            </div>
         </div>
     </div>
   )
