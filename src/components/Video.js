f59d92ba6f11e34cfc2ed339d679949d0162dc2e
BagirovM <109734813+BagirovM@users.noreply.github.com>
2022-08-19 23:01:10 +0400

    Update Video.js

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,28 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './VideoStyles.css'
+import './VidioStules.css'
+import BlueVidio from '../assets/Blue.mp4'
 
-
-import spaceVideo from '../assets/Blue.mp4'
-
-const Video = () => {
-    return (
-    <div className='back'>   
-        <div className='hero'>
-            <video autoPlay loop muted id='video'>
-                <source src={spaceVideo} type='video/mp4' />
-            </video>
-            <div className='content'>
-                <h1>Galaxy. Travel.</h1>
-                <p>World's first civilian space travel.</p>
-                <div>
-                    <Link to='/training' className='btn'><strong>Training</strong></Link>
-                    <Link to='/contact' className='btn btn-light'><strong>Launch</strong></Link>
-                </div>
-            </div>
+export const Vidio = () => {
+  return (
+    <div className='here'>
+        <video autoPlay loop muted id='video'>
+            <source src={BlueVidio} type='video/mp4'/>
+        </video>
+        <div className='content'>
+            <h1>Galaxy, Travel</h1>
+            <p>World's first civilian space travel.</p>
+        <div>
+            <Link to='/training' className='btn'>Training</Link>
+            <Link to='/contact' className='btn btn-light'>Launch</Link>
         </div>
-    </div> 
-    )
+        </div>
+     </div>
+    
+  )
 }
-
-export default Video
